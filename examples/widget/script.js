41bdca6298cdd245d716bc088151df7f9673c9fd
Florian Schnidrig <florian.schnidrig@coop.ch>
2026-03-27 13:04:07 +0100

    Persist data in local storage

--- examples/widget/script.js
+++ examples/widget/script.js
@@ -1,29 +1,45 @@
 // Get references to the DOM elements
-const motion      = document.getElementById("motion");
-const contrast    = document.getElementById("contrast");
-const colorscheme = document.getElementById("colorscheme");
+const motion      = document.getElementById('motion');
+const contrast    = document.getElementById('contrast');
+const colorscheme = document.getElementById('colorscheme');
 const root        = document.documentElement;
 
-// Users initial settings
-root.style.setProperty('--prefers-reduced-motion', window.matchMedia("(prefers-reduced-motion: reduce)").matches);
-root.style.setProperty('--prefers-contrast', window.matchMedia("(prefers-contrast: more)").matches);
-root.style.setProperty('--prefers-dark-theme', window.matchMedia("(prefers-color-scheme: dark)").matches);
+// Helper functions
+const setAccessibilityProperty = (property, value) => {
+    root.style.setProperty(property, value);
+    localStorage.setItem(property, value);
+}
 
-// Set initial values based on user settings (returns strings)
+const getAccessibilityProperty = (property, mediaQueryString) => {
+    const savedProperty = localStorage.getItem(property);
+
+    if(savedProperty) {
+        return savedProperty;
+    } else  {
+        return window.matchMedia(mediaQueryString).matches
+    }
+}
+
+// Users initial or saved settings
+setAccessibilityProperty('--prefers-reduced-motion', getAccessibilityProperty('--prefers-reduced-motion', '(prefers-reduced-motion: reduce)'));
+setAccessibilityProperty('--prefers-contrast',       getAccessibilityProperty('--prefers-contrast', '(prefers-contrast: more)'));
+setAccessibilityProperty('--prefers-dark-theme',     getAccessibilityProperty('--prefers-dark-theme', '(prefers-color-scheme: dark)'));
+
+// Set widget values based on user settings (returns strings)
 motion.checked      = getComputedStyle(root).getPropertyValue('--prefers-reduced-motion').trim() === 'true';
-contrast.checked    = getComputedStyle(root).getPropertyValue('--prefers-contrast').trim() === 'true';
-colorscheme.checked = getComputedStyle(root).getPropertyValue('--prefers-dark-theme').trim() === 'true';
+contrast.checked    = getComputedStyle(root).getPropertyValue('--prefers-contrast').trim()       === 'true';
+colorscheme.checked = getComputedStyle(root).getPropertyValue('--prefers-dark-theme').trim()     === 'true';
 
 
 // Change values when checkboxes change
-motion.addEventListener("change", () => {
-    root.style.setProperty('--prefers-reduced-motion', motion.checked);
+motion.addEventListener('change', () => {
+    setAccessibilityProperty('--prefers-reduced-motion', motion.checked);
 });
 
-contrast.addEventListener("change", () => {
-    root.style.setProperty('--prefers-contrast', contrast.checked);
+contrast.addEventListener('change', () => {
+    setAccessibilityProperty('--prefers-contrast', contrast.checked);
 });
 
-colorscheme.addEventListener("change", () => {
-    root.style.setProperty('--prefers-dark-theme', colorscheme.checked);
+colorscheme.addEventListener('change', () => {
+    setAccessibilityProperty('--prefers-dark-theme', colorscheme.checked);
 });
