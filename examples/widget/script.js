53cf7957735142603e0b6a4e658eda8e13b4fe93
Florian Schnidrig <florian.schnidrig@coop.ch>
2026-03-27 15:08:08 +0100

    Move from checkbox to radio input to allow more than 2 states and be more accessible

--- examples/widget/script.js
+++ examples/widget/script.js
@@ -1,45 +1,60 @@
 // Get references to the DOM elements
-const motion      = document.getElementById('motion');
-const contrast    = document.getElementById('contrast');
-const colorscheme = document.getElementById('colorscheme');
+const motion      = document.querySelectorAll('input[name="motion"]');
+const contrast    = document.querySelectorAll('input[name="contrast"]');
+const colorscheme = document.querySelectorAll('input[name="colorscheme"]');
 const root        = document.documentElement;
 
 // Helper functions
-const setAccessibilityProperty = (property, value) => {
-    root.style.setProperty(property, value);
+const setAccessibilityProperty = (property, value, mediaQueryString) => {
     localStorage.setItem(property, value);
+
+    // 'settings' is not a value we listen to in the CSS, as is can be anything, so we have to get the media query value
+    if(value === 'system') {
+        root.style.setProperty(property, window.matchMedia(mediaQueryString).matches);
+    } else {
+        root.style.setProperty(property, value);
+    }
 }
 
-const getAccessibilityProperty = (property, mediaQueryString) => {
+const getAccessibilityProperty = (property) => {
     const savedProperty = localStorage.getItem(property);
 
     if(savedProperty) {
         return savedProperty;
     } else  {
-        return window.matchMedia(mediaQueryString).matches
+        return 'system';
     }
 }
 
+const syncWidgetOption = (option, property) => {
+    const value = localStorage.getItem(property);
+
+    option.forEach(radio => {
+        radio.checked = radio.value === value;
+    });
+}
+
+const addOptionEventListener = (option, property, mediaQueryString) => {
+    option.forEach(radio => {
+        radio.addEventListener('change', () => {
+            if(radio.checked) {
+                setAccessibilityProperty(property, radio.value, mediaQueryString);
+            }
+        });
+    });
+}
+
 // Users initial or saved settings
-setAccessibilityProperty('--prefers-reduced-motion', getAccessibilityProperty('--prefers-reduced-motion', '(prefers-reduced-motion: reduce)'));
-setAccessibilityProperty('--prefers-contrast',       getAccessibilityProperty('--prefers-contrast', '(prefers-contrast: more)'));
-setAccessibilityProperty('--prefers-dark-theme',     getAccessibilityProperty('--prefers-dark-theme', '(prefers-color-scheme: dark)'));
+setAccessibilityProperty('--prefers-reduced-motion', getAccessibilityProperty('--prefers-reduced-motion'), '(prefers-reduced-motion: reduce)');
+setAccessibilityProperty('--prefers-contrast',       getAccessibilityProperty('--prefers-contrast'),       '(prefers-contrast: more)');
+setAccessibilityProperty('--prefers-dark-theme',     getAccessibilityProperty('--prefers-dark-theme'),     '(prefers-color-scheme: dark)');
 
 // Set widget values based on user settings (returns strings)
-motion.checked      = getComputedStyle(root).getPropertyValue('--prefers-reduced-motion').trim() === 'true';
-contrast.checked    = getComputedStyle(root).getPropertyValue('--prefers-contrast').trim()       === 'true';
-colorscheme.checked = getComputedStyle(root).getPropertyValue('--prefers-dark-theme').trim()     === 'true';
-
+syncWidgetOption(motion,      '--prefers-reduced-motion');
+syncWidgetOption(contrast,    '--prefers-contrast');
+syncWidgetOption(colorscheme, '--prefers-dark-theme');
 
 // Change values when checkboxes change
-motion.addEventListener('change', () => {
-    setAccessibilityProperty('--prefers-reduced-motion', motion.checked);
-});
-
-contrast.addEventListener('change', () => {
-    setAccessibilityProperty('--prefers-contrast', contrast.checked);
-});
-
-colorscheme.addEventListener('change', () => {
-    setAccessibilityProperty('--prefers-dark-theme', colorscheme.checked);
-});
+addOptionEventListener(motion,      '--prefers-reduced-motion', '(prefers-reduced-motion: reduce)');
+addOptionEventListener(contrast,    '--prefers-contrast',       '(prefers-contrast: more)');
+addOptionEventListener(colorscheme, '--prefers-dark-theme',     '(prefers-color-scheme: dark)');
