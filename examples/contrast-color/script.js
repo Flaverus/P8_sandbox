d2dc8ab7265e409bb4682cda637cf9a4ab22dcd3
Florian Schnidrig <florian.schnidrig@coop.ch>
2026-05-08 09:34:17 +0200

    Working on the contrast color CSS function to add custom saturation values

--- examples/contrast-color/script.js
+++ examples/contrast-color/script.js
@@ -1,7 +1,9 @@
 const colorPicker             = document.getElementById('base-color');
+const intensityPercentage     = document.getElementById('intensity-percentage');
 const pickedColorParagraph    = document.getElementById('picked-color-text');
 const contrastColor           = document.getElementById('contrast-color');
-const resultingColorParagraph = document.getElementById('resulting-color');
+const resultingColorParagraph = document.getElementById('resulting-color-text');
+const foregroundColorDiv      = document.getElementById('foreground-color');
 const root                    = document.documentElement;
 
 
@@ -11,4 +13,15 @@ colorPicker.addEventListener('change', () => {
     const color = colorPicker.value;
     root.style.setProperty('--selected-color', color);
     pickedColorParagraph.innerText = color;
+    updateResultingColorText();
 });
+
+intensityPercentage.addEventListener('change', () => {
+    const percentage = intensityPercentage.value;
+    root.style.setProperty('--selected-percentage', percentage + '%');
+    updateResultingColorText();
+});
+
+const updateResultingColorText = () => {
+    resultingColorParagraph.innerText = getComputedStyle(foregroundColorDiv).backgroundColor;
+};
